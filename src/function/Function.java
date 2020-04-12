b1700dad141bc8c70499a7a0e0b88d9f9303186b
Roman <Roman@Roman-DNS>
2020-04-12 17:27:54 +0500

    Fixed UTF-8

--- src/function/Function.java
+++ src/function/Function.java
@@ -57,7 +57,7 @@ public class Function extends Values {
 		} catch (Exception ex) {
 			// TODO: handle exception
 			logp(INFO, getClassName(Function.class), "clear", ex.getMessage());
-			errorMessage("Ошибка очистки списков!");
+			errorMessage("СњС€РёР±РєР° РѕС‡РёСЃС‚РєРё СЃРїРёСЃРєРѕРІ!");
 		}
 	}
 	
@@ -75,8 +75,8 @@ public class Function extends Values {
 		} catch (Exception ex) {
 			// TODO: handle exception
 			setError(true);
-			logp(INFO, getClassName(Function.class), "setArrayList", "Введите корректные значения!");
-			errorMessage("Введите корректные значения!");
+			logp(INFO, getClassName(Function.class), "setArrayList", "В¬РІРµРґРёС‚Рµ РєРѕСЂСЂРµРєС‚РЅС‹Рµ Р·РЅР°С‡РµРЅРёВ¤!");
+			errorMessage("В¬РІРµРґРёС‚Рµ РєРѕСЂСЂРµРєС‚РЅС‹Рµ Р·РЅР°С‡РµРЅРёВ¤!");
 		}
 	}
 	
@@ -84,7 +84,7 @@ public class Function extends Values {
 		// TODO Auto-generated method stub
 		if (isError()) {
 			setError(false);
-			logp(INFO, getClassName(Function.class), "check", "Обнуление ошибки");
+			logp(INFO, getClassName(Function.class), "check", "СњР±РЅСѓР»РµРЅРёРµ РѕС€РёР±РєРё");
 		}
 	}
 
