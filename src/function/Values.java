1541ec4faf3e6857fe40937cd85787d80e093e4b
glush <glush@LAPTOP-AREM3LPN.Dlink>
2020-04-12 19:24:08 +0500

    18130166

--- src/function/Values.java
+++ src/function/Values.java
@@ -1,23 +1,21 @@
 package function;
 
 import java.util.ArrayList;
-
 /**
 * @author 18130166
 */
-
 public class Values {
 
-private static ArrayList<Boolean> a0 = new ArrayList<Boolean>();	//	"Коллекция переключателей va"			
-private static ArrayList<Double>  a1 = new ArrayList<Double>();		//	"Коллекция тарифы все vb"				
-private static ArrayList<Double>  a2 = new ArrayList<Double>();		//	"Коллекция вводимых значений vc"		
-private static ArrayList<Double>  a3 = new ArrayList<Double>();		//	"Коллекция итого ve"					
-private static ArrayList<String>  a4 = new ArrayList<String>();		//	"Коллекция вывода vf"					
-private static ArrayList<Double>  b1 = new ArrayList<Double>();		//	"Коллекция тарифы электричество ea"	
-private static ArrayList<Double>  b2 = new ArrayList<Double>();		//	"Коллекция значения электричество eb"	
-private static ArrayList<Double>  b3 = new ArrayList<Double>();		//	"Коллекция итого электричество ec"	
-private static ArrayList<String>  b4 = new ArrayList<String>();		//	"Коллекция вывод электричество ef"	
-private static double value;										//	"Итог"
+private static ArrayList<Boolean> a0 = new ArrayList<Boolean>();	//Коллекция переключателей va
+private static ArrayList<Double>  a1 = new ArrayList<Double>();		//Коллекция тарифы все vb
+private static ArrayList<Double>  a2 = new ArrayList<Double>();		//Коллекция вводимых значений vc
+private static ArrayList<Double>  a3 = new ArrayList<Double>();		//Коллекция итого ve
+private static ArrayList<String>  a4 = new ArrayList<String>();		//Коллекция вывода vf
+private static ArrayList<Double>  b1 = new ArrayList<Double>();		//Коллекция тарифы электричество ea
+private static ArrayList<Double>  b2 = new ArrayList<Double>();		//Коллекция значения электричество eb
+private static ArrayList<Double>  b3 = new ArrayList<Double>();		//Коллекция итого электричество ec
+private static ArrayList<String>  b4 = new ArrayList<String>();		//Коллекция вывод электричество ef
+private static double value;										//Итог								
 
 /**
  * @return the a0
@@ -147,3 +145,4 @@ private static double value;										//	"Итог"
 	}
 }
 
+//конец 
