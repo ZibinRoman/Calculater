bab31f3de89fab1ea838cc92764f978d45a81442
glush <glush@192.168.0.77>
2020-05-16 13:15:44 +0500

    166

--- src/main/java/database/Output.java
+++ src/main/java/database/Output.java
@@ -1,9 +1,9 @@
 /**
- * пакет database;
+ * РїР°РєРµС‚ database;
  * */
 package database;
 /**
- * импортируем все необходимое для класса
+ * РёРјРїРѕСЂС‚РёСЂСѓРµРј РІСЃРµ РЅРµРѕР±С…РѕРґРёРјРѕРµ РґР»СЏ РєР»Р°СЃСЃР°
  * */
 import static java.lang.String.*;
 import static database.Strings.*;
@@ -12,54 +12,54 @@ import java.util.*;
 import javax.swing.*;
 import function.*;
 /**
- * класс для заполнения платежной квитанции в файл Output.txt
+ * РєР»Р°СЃСЃ РґР»СЏ Р·Р°РїРѕР»РЅРµРЅРёСЏ РїР»Р°С‚РµР¶РЅРѕР№ РєРІРёС‚Р°РЅС†РёРё РІ С„Р°Р№Р» Output.txt
  * */
 public class Output extends Values{
 
 	public Output(ArrayList<JCheckBox> checkBoxList, ArrayList<Double> doubleListTarif, ArrayList<Double> doubleListValue, ArrayList<Double> doubleListTotal, ArrayList<Double> doubleListTarifEnergy, ArrayList<Double> doubleListValueEnergy, ArrayList<Double> doubleListTotalEnergy, int toggle, double value) {
 		super(checkBoxList, doubleListValue, doubleListValueEnergy, toggle);
-		this.setDoubleListTarif(doubleListTarif);// инициализация переменных для вывода в файл
-		this.setDoubleListTotal(doubleListTotal);// инициализация переменных для вывода в файл
-		this.setDoubleListTarifEnergy(doubleListTarifEnergy);// инициализация переменных для вывода в файл
-		this.setDoubleListTotalEnergy(doubleListTotalEnergy);// инициализация переменных для вывода в файл
-		this.setValue(value);// инициализация переменных для вывода в файл
+		this.setDoubleListTarif(doubleListTarif);// РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅС‹С… РґР»СЏ РІС‹РІРѕРґР° РІ С„Р°Р№Р»
+		this.setDoubleListTotal(doubleListTotal);// РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅС‹С… РґР»СЏ РІС‹РІРѕРґР° РІ С„Р°Р№Р»
+		this.setDoubleListTarifEnergy(doubleListTarifEnergy);// РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅС‹С… РґР»СЏ РІС‹РІРѕРґР° РІ С„Р°Р№Р»
+		this.setDoubleListTotalEnergy(doubleListTotalEnergy);// РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅС‹С… РґР»СЏ РІС‹РІРѕРґР° РІ С„Р°Р№Р»
+		this.setValue(value);// РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ РїРµСЂРµРјРµРЅРЅС‹С… РґР»СЏ РІС‹РІРѕРґР° РІ С„Р°Р№Р»
 	}
 	/**
-	 * построчный вывод в файл Output.txt
+	 * РїРѕСЃС‚СЂРѕС‡РЅС‹Р№ РІС‹РІРѕРґ РІ С„Р°Р№Р» Output.txt
 	 * */
 	public void initializeOutput() {
 		try {
 			initializeList(getStringList(), getStringListEnergy(), getToggle());
 			BufferedWriter bufferedwriter = new BufferedWriter(new FileWriter(getPathtooutput()));
 			for (int i = 0; i < getStringList().size(); i++) {
-				bufferedwriter.write(getWrite(valueOf(getDoubleListTarif().get(i)), valueOf(getDoubleListValue().get(i)), valueOf(getDoubleListTotal().get(i)), getStringList().get(i)));//Записывает
-				bufferedwriter.newLine();//Записывает разделитель строк
+				bufferedwriter.write(getWrite(valueOf(getDoubleListTarif().get(i)), valueOf(getDoubleListValue().get(i)), valueOf(getDoubleListTotal().get(i)), getStringList().get(i)));//Р—Р°РїРёСЃС‹РІР°РµС‚
+				bufferedwriter.newLine();//Р—Р°РїРёСЃС‹РІР°РµС‚ СЂР°Р·РґРµР»РёС‚РµР»СЊ СЃС‚СЂРѕРє
 			}
 			for (int i = 0; i < getStringListEnergy().size(); i++) {
-				bufferedwriter.write(getWrite(valueOf(getDoubleListTarifEnergy().get(i)), valueOf(getDoubleListValueEnergy().get(i)), valueOf(getDoubleListTotalEnergy().get(i)), getStringListEnergy().get(i)));//Записывает 
-				bufferedwriter.newLine();//Записывает разделитель строк
+				bufferedwriter.write(getWrite(valueOf(getDoubleListTarifEnergy().get(i)), valueOf(getDoubleListValueEnergy().get(i)), valueOf(getDoubleListTotalEnergy().get(i)), getStringListEnergy().get(i)));//Р—Р°РїРёСЃС‹РІР°РµС‚ 
+				bufferedwriter.newLine();//Р—Р°РїРёСЃС‹РІР°РµС‚ СЂР°Р·РґРµР»РёС‚РµР»СЊ СЃС‚СЂРѕРє
 			}
-			bufferedwriter.write(join(getEmpty(), format(getFormat80(), String.join(getSpace(), Strings.getTotal(), "к", "оплате".concat(getColon()))),  format(getFormat10(), valueOf(getValue()))));//Записывает
-			bufferedwriter.newLine();//Записывает разделитель строк
-			bufferedwriter.write(join(getEmpty(), String.format(getFormat80(),  Strings.getTimer().concat(getColon())), format(getFormat30(), getDate())));//Записывает
-			bufferedwriter.close();//Закрывает буфер
+			bufferedwriter.write(join(getEmpty(), format(getFormat80(), String.join(getSpace(), Strings.getTotal(), "Рє", "РѕРїР»Р°С‚Рµ".concat(getColon()))),  format(getFormat10(), valueOf(getValue()))));//Р—Р°РїРёСЃС‹РІР°РµС‚
+			bufferedwriter.newLine();//Р—Р°РїРёСЃС‹РІР°РµС‚ СЂР°Р·РґРµР»РёС‚РµР»СЊ СЃС‚СЂРѕРє
+			bufferedwriter.write(join(getEmpty(), String.format(getFormat80(),  Strings.getTimer().concat(getColon())), format(getFormat30(), getDate())));//Р—Р°РїРёСЃС‹РІР°РµС‚
+			bufferedwriter.close();//Р—Р°РєСЂС‹РІР°РµС‚ Р±СѓС„РµСЂ
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	/**
-	 * getDate() берет системную дату
-	 * и преобразует ее в строку для вывода 
-	 * в документ Output.txt
+	 * getDate() Р±РµСЂРµС‚ СЃРёСЃС‚РµРјРЅСѓСЋ РґР°С‚Сѓ
+	 * Рё РїСЂРµРѕР±СЂР°Р·СѓРµС‚ РµРµ РІ СЃС‚СЂРѕРєСѓ РґР»СЏ РІС‹РІРѕРґР° 
+	 * РІ РґРѕРєСѓРјРµРЅС‚ Output.txt
 	 * */
 	private static String getDate() {
 		Date date = new Date();
 		return date.toString();
 	}
 	/**
-	 * объединяет переменные с разделителем пусто ""
-	 * и записывает в переменную 
-	 * для последующего вывода в документ
+	 * РѕР±СЉРµРґРёРЅСЏРµС‚ РїРµСЂРµРјРµРЅРЅС‹Рµ СЃ СЂР°Р·РґРµР»РёС‚РµР»РµРј РїСѓСЃС‚Рѕ ""
+	 * Рё Р·Р°РїРёСЃС‹РІР°РµС‚ РІ РїРµСЂРµРјРµРЅРЅСѓСЋ 
+	 * РґР»СЏ РїРѕСЃР»РµРґСѓСЋС‰РµРіРѕ РІС‹РІРѕРґР° РІ РґРѕРєСѓРјРµРЅС‚
 	 * */
 	private String getWrite(String doubleListTarif, String doubleListValue, String doubleListTotal, String stringList) {
 		String string0 = format(getFormat80(), stringList);
@@ -69,56 +69,56 @@ public class Output extends Values{
 		return join(getEmpty(),  string0, string1, string2, string3);
 	}
 	/**
-	 * строки для заполнения 
-	 * документа - платежной квитанции
+	 * СЃС‚СЂРѕРєРё РґР»СЏ Р·Р°РїРѕР»РЅРµРЅРёСЏ 
+	 * РґРѕРєСѓРјРµРЅС‚Р° - РїР»Р°С‚РµР¶РЅРѕР№ РєРІРёС‚Р°РЅС†РёРё
 	 * */
 	private void initializeList(ArrayList<String> stringList, ArrayList<String> stringListEnergy, int toggle) {
 		try {
 			/**
-			 * названия тарифов
+			 * РЅР°Р·РІР°РЅРёСЏ С‚Р°СЂРёС„РѕРІ
 			 * */
 			stringList.clear();
 			stringListEnergy.clear();
-			stringList.add(0, "Содержание");
-			stringList.add(1, "Уборка мест общего использования");
-			stringList.add(2, "Холодное водоснабжение при содержании общего имущества");
-			stringList.add(3, "Холодная вода на горячее водоснабжение при содержании общего имущества");
-			stringList.add(4, "Тепловая энергия горячего водоснабжения при содержании общего имущества");
-			stringList.add(5, "Отведение сточных вод холодной воды при содержании общего имущества");
-			stringList.add(6, "Отведение сточных вод горячей воды при содержании общего имущества");
-			stringList.add(7, "Электроэнергия при содержании общего имущества");
-			stringList.add(8, "Отопление");
-			stringList.add(9, "Холодное водоснабжение");
-			stringList.add(10, "Холодное водоснабжение на нужды горячего водоснабжения");
-			stringList.add(11, "Тепловая энергия горячего водоснабжения");
-			stringList.add(12, "Отведение сточных вод холодного водоснабжения");
-			stringList.add(13, "Отведение сточных вод горячего водоснабжения");
-			stringList.add(14, "Обращение с твердыми коммунальными отходами");
-			stringList.add(15, "Обслуживание телевизионной антенны");
-			stringList.add(16, "Замочно-переговорное устройство");
-			stringList.add(17, "Радио");
-			stringList.add(18, "Газ");
+			stringList.add(0, "РЎРѕРґРµСЂР¶Р°РЅРёРµ");
+			stringList.add(1, "РЈР±РѕСЂРєР° РјРµСЃС‚ РѕР±С‰РµРіРѕ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ");
+			stringList.add(2, "РҐРѕР»РѕРґРЅРѕРµ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёРµ РїСЂРё СЃРѕРґРµСЂР¶Р°РЅРёРё РѕР±С‰РµРіРѕ РёРјСѓС‰РµСЃС‚РІР°");
+			stringList.add(3, "РҐРѕР»РѕРґРЅР°СЏ РІРѕРґР° РЅР° РіРѕСЂСЏС‡РµРµ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёРµ РїСЂРё СЃРѕРґРµСЂР¶Р°РЅРёРё РѕР±С‰РµРіРѕ РёРјСѓС‰РµСЃС‚РІР°");
+			stringList.add(4, "РўРµРїР»РѕРІР°СЏ СЌРЅРµСЂРіРёСЏ РіРѕСЂСЏС‡РµРіРѕ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёСЏ РїСЂРё СЃРѕРґРµСЂР¶Р°РЅРёРё РѕР±С‰РµРіРѕ РёРјСѓС‰РµСЃС‚РІР°");
+			stringList.add(5, "РћС‚РІРµРґРµРЅРёРµ СЃС‚РѕС‡РЅС‹С… РІРѕРґ С…РѕР»РѕРґРЅРѕР№ РІРѕРґС‹ РїСЂРё СЃРѕРґРµСЂР¶Р°РЅРёРё РѕР±С‰РµРіРѕ РёРјСѓС‰РµСЃС‚РІР°");
+			stringList.add(6, "РћС‚РІРµРґРµРЅРёРµ СЃС‚РѕС‡РЅС‹С… РІРѕРґ РіРѕСЂСЏС‡РµР№ РІРѕРґС‹ РїСЂРё СЃРѕРґРµСЂР¶Р°РЅРёРё РѕР±С‰РµРіРѕ РёРјСѓС‰РµСЃС‚РІР°");
+			stringList.add(7, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ РїСЂРё СЃРѕРґРµСЂР¶Р°РЅРёРё РѕР±С‰РµРіРѕ РёРјСѓС‰РµСЃС‚РІР°");
+			stringList.add(8, "РћС‚РѕРїР»РµРЅРёРµ");
+			stringList.add(9, "РҐРѕР»РѕРґРЅРѕРµ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёРµ");
+			stringList.add(10, "РҐРѕР»РѕРґРЅРѕРµ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёРµ РЅР° РЅСѓР¶РґС‹ РіРѕСЂСЏС‡РµРіРѕ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёСЏ");
+			stringList.add(11, "РўРµРїР»РѕРІР°СЏ СЌРЅРµСЂРіРёСЏ РіРѕСЂСЏС‡РµРіРѕ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёСЏ");
+			stringList.add(12, "РћС‚РІРµРґРµРЅРёРµ СЃС‚РѕС‡РЅС‹С… РІРѕРґ С…РѕР»РѕРґРЅРѕРіРѕ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёСЏ");
+			stringList.add(13, "РћС‚РІРµРґРµРЅРёРµ СЃС‚РѕС‡РЅС‹С… РІРѕРґ РіРѕСЂСЏС‡РµРіРѕ РІРѕРґРѕСЃРЅР°Р±Р¶РµРЅРёСЏ");
+			stringList.add(14, "РћР±СЂР°С‰РµРЅРёРµ СЃ С‚РІРµСЂРґС‹РјРё РєРѕРјРјСѓРЅР°Р»СЊРЅС‹РјРё РѕС‚С…РѕРґР°РјРё");
+			stringList.add(15, "РћР±СЃР»СѓР¶РёРІР°РЅРёРµ С‚РµР»РµРІРёР·РёРѕРЅРЅРѕР№ Р°РЅС‚РµРЅРЅС‹");
+			stringList.add(16, "Р—Р°РјРѕС‡РЅРѕ-РїРµСЂРµРіРѕРІРѕСЂРЅРѕРµ СѓСЃС‚СЂРѕР№СЃС‚РІРѕ");
+			stringList.add(17, "Р Р°РґРёРѕ");
+			stringList.add(18, "Р“Р°Р·");
 			switch (toggle) {
 			/**
-			 * тарифная зона 1
+			 * С‚Р°СЂРёС„РЅР°СЏ Р·РѕРЅР° 1
 			 * */
 			case 1:
-				stringListEnergy.add(0, "Электроэнергия Одноставочный тариф");
+				stringListEnergy.add(0, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ РћРґРЅРѕСЃС‚Р°РІРѕС‡РЅС‹Р№ С‚Р°СЂРёС„");
 				break;
 			/**
-			 * тарифная зона 1
+			 * С‚Р°СЂРёС„РЅР°СЏ Р·РѕРЅР° 1
 			 * */
 			case 2:
-				stringListEnergy.add(0, "Электроэнергия. Дневная зона");
-				stringListEnergy.add(1, "Электроэнергия. Ночная зона");
+				stringListEnergy.add(0, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ. Р”РЅРµРІРЅР°СЏ Р·РѕРЅР°");
+				stringListEnergy.add(1, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ. РќРѕС‡РЅР°СЏ Р·РѕРЅР°");
 				break;
 			/**
-			 * тарифная зона 1
+			 * С‚Р°СЂРёС„РЅР°СЏ Р·РѕРЅР° 1
 			 * */
 			case 3:
-				stringListEnergy.add(0, "Электроэнергия. Пиковая зона");
-				stringListEnergy.add(1, "Электроэнергия. Полупиковая зона");
-				stringListEnergy.add(2, "Электроэнергия. Ночная зона");
+				stringListEnergy.add(0, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ. РџРёРєРѕРІР°СЏ Р·РѕРЅР°");
+				stringListEnergy.add(1, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ. РџРѕР»СѓРїРёРєРѕРІР°СЏ Р·РѕРЅР°");
+				stringListEnergy.add(2, "Р­Р»РµРєС‚СЂРѕСЌРЅРµСЂРіРёСЏ. РќРѕС‡РЅР°СЏ Р·РѕРЅР°");
 				break;
 			default:
 				break;
